b65c8c854dba4c6bd3a2f54673cd9caa5e4da3ce
Wei Mingzhi <weimingzhi@gmail.com>
2013-02-16 10:55:25 +0800

    Keycode

--- common.h
+++ common.h
@@ -47,6 +47,19 @@ extern "C"
 #define realloc      SDL_realloc
 #endif
 
+#if SDL_VERSION_ATLEAST(2,0,0)
+#define SDLK_KP1     SDLK_KP_1
+#define SDLK_KP2     SDLK_KP_2
+#define SDLK_KP3     SDLK_KP_3
+#define SDLK_KP4     SDLK_KP_4
+#define SDLK_KP5     SDLK_KP_5
+#define SDLK_KP6     SDLK_KP_6
+#define SDLK_KP7     SDLK_KP_7
+#define SDLK_KP8     SDLK_KP_8
+#define SDLK_KP9     SDLK_KP_9
+#define SDLK_KP0     SDLK_KP_0
+#endif
+
 #if SDL_BYTEORDER == SDL_LIL_ENDIAN
 #define SWAP16(X)    (X)
 #define SWAP32(X)    (X)
