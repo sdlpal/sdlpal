966a775da0642bd0eab47ca4c6c194447a7ac555
louyihua <louyihua@21cn.com>
2016-06-01 17:14:07 +0800

    Android Makefile update

--- android/jni/src/Android.mk
+++ android/jni/src/Android.mk
@@ -1,19 +1,21 @@
-LOCAL_PATH := $(call my-dir)
+SDLPAL_LOCAL_PATH := $(call my-dir)
+SDL_PATH := $(SDLPAL_LOCAL_PATH)/../../../SDL2
+
+include $(SDL_PATH)/Android.mk
 
 include $(CLEAR_VARS)
-
+LOCAL_PATH := $(SDLPAL_LOCAL_PATH)
 LOCAL_MODULE := main
 
-SDL_PATH := ../../../SDL2
 OGG_PATH := ../../../liboggvorbis
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(OGG_PATH)/include $(LOCAL_PATH)/$(OGG_PATH)/src
 
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-    $(wildcard ../../*.cpp) $(wildcard ../../*.c) \
-    $(wildcard ../../adplug/*.c) $(wildcard ../../adplug/*.cpp) \
-    $(wildcard ../../liboggvorbis/src/*.c) \
-    $(wildcard ../../libmad/*.c)
+    $(wildcard $(LOCAL_PATH)/../../*.cpp) $(wildcard $(LOCAL_PATH)/../../*.c) \
+    $(wildcard $(LOCAL_PATH)/../../adplug/*.c) $(wildcard $(LOCAL_PATH)/../../adplug/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/../../liboggvorbis/src/*.c) \
+    $(wildcard $(LOCAL_PATH)/../../libmad/*.c)
 
 LOCAL_CFLAGS += -std=c99
 
