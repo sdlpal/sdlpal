5c9894eef6b8871835569f1b953e9ed5eded776d
LouYihua <supermouselyh@hotmail.com>
2017-02-22 23:18:16 +0800

    Fix unix-like builds

--- android/jni/src/Android.mk
+++ android/jni/src/Android.mk
@@ -1,21 +1,19 @@
-SDLPAL_LOCAL_PATH := $(call my-dir)
-SDL_PATH := $(SDLPAL_LOCAL_PATH)/../../../SDL2
+CURRENT_PATH := $(call my-dir)
 
+SDL_PATH := $(CURRENT_PATH)/../../../SDL2
 include $(SDL_PATH)/Android.mk
 
 include $(CLEAR_VARS)
-LOCAL_PATH := $(SDLPAL_LOCAL_PATH)
+LOCAL_PATH := $(CURRENT_PATH)/../../..
 LOCAL_MODULE := main
 
-OGG_PATH := ../../../liboggvorbis
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(OGG_PATH)/include $(LOCAL_PATH)/$(OGG_PATH)/src
+LOCAL_C_INCLUDES := $(CURRENT_PATH) $(LOCAL_PATH) $(SDL_PATH)/include $(LOCAL_PATH)/src/liboggvorbis/include $(LOCAL_PATH)/src/liboggvorbis/src
 
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-    $(wildcard $(LOCAL_PATH)/../../../*.cpp) $(wildcard $(LOCAL_PATH)/../../../*.c) \
-    $(wildcard $(LOCAL_PATH)/../../../adplug/*.c) $(wildcard $(LOCAL_PATH)/../../../adplug/*.cpp) \
-    $(wildcard $(LOCAL_PATH)/../../../liboggvorbis/src/*.c) \
-    $(wildcard $(LOCAL_PATH)/../../../libmad/*.c)
+    $(wildcard $(LOCAL_PATH)/src/*.cpp) $(wildcard $(LOCAL_PATH)/src/*.c) \
+    $(wildcard $(LOCAL_PATH)/src/adplug/*.c) $(wildcard $(LOCAL_PATH)/src/adplug/*.cpp) \
+    $(wildcard $(LOCAL_PATH)/src/liboggvorbis/src/*.c) \
+    $(wildcard $(LOCAL_PATH)/src/libmad/*.c)
 
 LOCAL_CFLAGS += -std=c99
 
