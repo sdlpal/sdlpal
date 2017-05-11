f66fd8da6407c1427adc1500ede940660aac19de
Pal Lockheart <ex@palx.org>
2017-05-12 05:23:37 +0800

    Log rev: macOS

---
+++ macos/util.mm
@@ -0,0 +1,40 @@
+#include <Foundation/Foundation.h>
+#include "palcfg.h"
+#include "util.h"
+
+BOOL
+UTIL_GetScreenSize(
+                   DWORD *pdwScreenWidth,
+                   DWORD *pdwScreenHeight
+                   )
+{
+    return (pdwScreenWidth && pdwScreenHeight && *pdwScreenWidth && *pdwScreenHeight);
+}
+
+BOOL
+UTIL_IsAbsolutePath(
+                    LPCSTR  lpszFileName
+                    )
+{
+    return FALSE;
+}
+
+INT
+UTIL_Platform_Init(
+                   int argc,
+                   char* argv[]
+                   )
+{
+    UTIL_LogAddOutputCallback([](LOGLEVEL, const char* str, const char*)->void {
+        NSLog(@"%s",str);
+    });
+    gConfig.fLaunchSetting = FALSE;
+    return 0;
+}
+
+VOID
+UTIL_Platform_Quit(
+                   VOID
+                   )
+{
+}
