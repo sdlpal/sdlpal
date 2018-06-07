ea217e29551e1e846676475d854d7bec06b1ff36
Lou Yihua <supermouselyh@hotmail.com>
2018-06-07 22:45:02 +0800

    Message optimize in git revision generating

--- scripts/gengitrev.bat
+++ scripts/gengitrev.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal enableextensions
 setlocal enabledelayedexpansion
-for /f %%t in ('git describe --tags') do set PAL_GIT_REVISION=%%t
+for /f %%t in ('git describe --tags --dirty=-dirty') do set PAL_GIT_REVISION=%%t
 if "!PAL_GIT_REVISION!" == "" (
 	for %%t in (%~dp0..\.gitignore) do set PAL_GIT_REVISION=%%~tt
 )
 findstr /c:"#define PAL_GIT_REVISION \"!PAL_GIT_REVISION!\"" "%~dp0..\generated.h" >nul 2>&1
 if errorlevel 1 (
-    echo Generating Git revision header... 1>&2
+    echo Generating Git revision header: !PAL_GIT_REVISION! 1>&2
     echo #define PAL_GIT_REVISION "!PAL_GIT_REVISION!" > "%~dp0..\generated.h"
 )
 exit /b 0
