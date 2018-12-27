sdlpal/sdlpal

commit 8072af34a2aa67d001ec479d96bcbcbaf5e36ffa

vanilla: player weak will not wobble when confused

battle.c
@@ -191,6 +191,7 @@ PAL_BattleMakeScene(
             //
             // Player is confused
             //
+            if(!PAL_IsPlayerDying(gpGlobals->rgParty[i].wPlayerRole))
             pos = PAL_XY(PAL_X(g_Battle.rgPlayer[i].pos), PAL_Y(g_Battle.rgPlayer[i].pos) + RandomLong(-1, 1));
             pos = PAL_XY(PAL_X(pos) - PAL_RLEGetWidth(PAL_SpriteGetFrame(g_Battle.rgPlayer[i].lpSprite, g_Battle.rgPlayer[i].wCurrentFrame)) / 2,
                PAL_Y(pos) - PAL_RLEGetHeight(PAL_SpriteGetFrame(g_Battle.rgPlayer[i].lpSprite, g_Battle.rgPlayer[i].wCurrentFrame)));

commit f56ee4e3fd995c366109a8f44dc6d1bed4a6375e

fix wobble pos wrong when dying introduced in 8072af3

battle.c
@@ -191,8 +191,10 @@ PAL_BattleMakeScene(
             //
             // Player is confused
             //
+            int xd = PAL_X(g_Battle.rgPlayer[i].pos), yd = PAL_Y(g_Battle.rgPlayer[i].pos);
             if(!PAL_IsPlayerDying(gpGlobals->rgParty[i].wPlayerRole))
-            pos = PAL_XY(PAL_X(g_Battle.rgPlayer[i].pos), PAL_Y(g_Battle.rgPlayer[i].pos) + RandomLong(-1, 1));
+               yd += RandomLong(-1, 1);
+            pos = PAL_XY(xd, yd);
             pos = PAL_XY(PAL_X(pos) - PAL_RLEGetWidth(PAL_SpriteGetFrame(g_Battle.rgPlayer[i].lpSprite, g_Battle.rgPlayer[i].wCurrentFrame)) / 2,
                PAL_Y(pos) - PAL_RLEGetHeight(PAL_SpriteGetFrame(g_Battle.rgPlayer[i].lpSprite, g_Battle.rgPlayer[i].wCurrentFrame)));
 