2aeda5a6d93590b4553e3ecf6197d736783478e3
PalMusicFan <25916579+PalMusicFan@users.noreply.github.com>
2019-07-26 13:09:59 +0800

    feat: new localization file extension. Thanks to @palxex

---
+++ defines.h
@@ -0,0 +1,30 @@
+/* -*- mode: c; tab-width: 4; c-basic-offset: 4; c-file-style: "linux" -*- */
+//
+// Copyright (c) 2009-2011, Wei Mingzhi <whistler_wmz@users.sf.net>.
+// Copyright (c) 2011-2019, SDLPAL development team.
+// All rights reserved.
+//
+// This file is part of SDLPAL.
+//
+// SDLPAL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _DEFINES_H
+#define _DEFINES_H
+
+#ifndef PAL_LOCALIZATION_EXT
+# define PAL_LOCALIZATION_EXT "slf"
+#endif
+
+#endif //_DEFINES_H
