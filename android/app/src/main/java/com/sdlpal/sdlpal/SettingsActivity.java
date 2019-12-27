sdlpal/sdlpal

commit fac322608c7cc591ef9b50ad306fc2bc50ce6678

palcfg & GUI: add OPUS; enable no CD( then no fallback, avoid crashing on specific platforms )

android/app/src/main/java/com/sdlpal/sdlpal/SettingsActivity.java
@@ -95,8 +95,8 @@ public class SettingsActivity extends AppCompatActivity {
     private static final int AudioSampleRates[] = { 11025, 22050, 44100 };
     private static final int AudioBufferSizes[] = { 512, 1024, 2048, 4096, 8192 };
     private static final int OPLSampleRates[] = { 11025, 12429, 22050, 24858, 44100, 49716 };
-    private static final String CDFormats[] = { "MP3", "OGG" };
-    private static final String MusicFormats[] = { "MIDI", "RIX", "MP3", "OGG" };
+    private static final String CDFormats[] = { "None", "MP3", "OGG", "OPUS" };
+    private static final String MusicFormats[] = { "MIDI", "RIX", "MP3", "OGG", "OPUS" };
     private static final String OPLCores[] = { "MAME", "DBFLT", "DBINT", "NUKED" };
     private static final String OPLChips[] = { "OPL2", "OPL3" };
     private static final String AspectRatios[] = { "16:10", "4:3" };
@@ -358,7 +358,7 @@ public class SettingsActivity extends AppCompatActivity {
         ((AppCompatSpinner)findViewById(R.id.spLogLevel)).setSelection(getConfigInt(LogLevel, true));
         ((AppCompatSpinner)findViewById(R.id.spSample)).setSelection(findMatchedIntIndex(getConfigInt(SampleRate, true), AudioSampleRates, 2));    // 44100Hz
         ((AppCompatSpinner)findViewById(R.id.spBuffer)).setSelection(findMatchedIntIndex(getConfigInt(AudioBufferSize, true), AudioBufferSizes, 1));    // 1024
-        ((AppCompatSpinner)findViewById(R.id.spCDFmt)).setSelection(findMatchedStringIndex(getConfigString(CDFormat, true), CDFormats, 1));     // OGG
+        ((AppCompatSpinner)findViewById(R.id.spCDFmt)).setSelection(findMatchedStringIndex(getConfigString(CDFormat, true), CDFormats, 1));     // None
         ((AppCompatSpinner)findViewById(R.id.spMusFmt)).setSelection(findMatchedStringIndex(getConfigString(MusicFormat, true), MusicFormats, 1));    // RIX
         ((AppCompatSpinner)findViewById(R.id.spOPLCore)).setSelection(findMatchedStringIndex(getConfigString(OPLCore, true), OPLCores, 1));       // DBFLT
         ((AppCompatSpinner)findViewById(R.id.spOPLChip)).setSelection(findMatchedStringIndex(getConfigString(OPLChip, true), OPLChips, 0));       // OPL2

audio.c
@@ -184,11 +184,7 @@ AUDIO_CD_Available(
    VOID
 )
 {
-#if PAL_HAS_SDLCD
-   return gAudioDevice.pCD != NULL;
-#else
-   return gAudioDevice.pCDPlayer != NULL;
-#endif
+   return gConfig.eCDType != CD_NONE;
 }
 
 INT
@@ -313,7 +309,7 @@ AUDIO_OpenDevice(
    //
    switch (gConfig.eCDType)
    {
-   case MUSIC_SDLCD:
+   case CD_SDLCD:
    {
 #if PAL_HAS_SDLCD
 	   int i;
@@ -339,16 +335,17 @@ AUDIO_OpenDevice(
 	   gAudioDevice.pCDPlayer = NULL;
 	   break;
    }
-   case MUSIC_MP3:
+   case CD_MP3:
 	   gAudioDevice.pCDPlayer = MP3_Init();
 	   break;
-   case MUSIC_OGG:
+   case CD_OGG:
 	   gAudioDevice.pCDPlayer = OGG_Init();
 	   break;
-   case MUSIC_OPUS:
+   case CD_OPUS:
 	   gAudioDevice.pCDPlayer = OPUS_Init();
 	   break;
    default:
+      gAudioDevice.pCDPlayer = NULL;
 	   break;
    }
 

ios/SDLPal/SDLPal/SettingsTableViewController.m
@@ -94,8 +94,8 @@
     AudioSampleRates = @[ @"11025", @"22050", @"44100", @"49716" ];
     AudioBufferSizes = @[ @"512", @"1024", @"2048", @"4096", @"8192" ];
     OPLSampleRates = @[ @"12429", @"24858", @"49716", @"11025", @"22050", @"44100" ];
-    CDFormats = @[ @"MP3", @"OGG" ];
-    MusicFormats = @[ @"MIDI", @"RIX", @"MP3", @"OGG" ];
+    CDFormats = @[ @"None", @"MP3", @"OGG", @"OPUS" ];
+    MusicFormats = @[ @"MIDI", @"RIX", @"MP3", @"OGG", @"OPUS" ];
     OPLCores = @[ @"MAME", @"DBFLT", @"DBINT", @"NUKED" ];
     OPLChips = @[ @"OPL2", @"OPL3" ];
     LogLevels = @[ @"VERBOSE", @"DEBUG", @"INFO", @"WARNING", @"ERROR", @"FATAL" ];
@@ -316,7 +316,7 @@ typedef void(^SelectedBlock)(NSString *selected);
     lblOPLCore.text         = OPLCores[gConfig.eOPLCore];
     lblOPLChip.text         = OPLChips[gConfig.eOPLChip];
     lblOPLRate.text         = [NSString stringWithFormat:@"%d",gConfig.iOPLSampleRate];
-    lblCDAudioSource.text   = CDFormats[gConfig.eCDType-MUSIC_OGG];
+    lblCDAudioSource.text   = CDFormats[gConfig.eCDType];
     lblResampleRate.text    = [NSString stringWithFormat:@"%d",gConfig.iSampleRate];
     lblAudioBufferSize.text = [NSString stringWithFormat:@"%d",gConfig.wAudioBufferSize];
     lblShader.text          = [NSString stringWithFormat:@"%s",gConfig.pszShader];
@@ -353,7 +353,7 @@ typedef void(^SelectedBlock)(NSString *selected);
     gConfig.eOPLCore    = (OPLCORE_TYPE)[OPLCores  indexOfObject:lblOPLCore.text];
     gConfig.eOPLChip    = gConfig.eOPLCore == OPLCORE_NUKED ? OPLCHIP_OPL3 : (OPLCHIP_TYPE)[OPLChips  indexOfObject:lblOPLChip.text];
     gConfig.iOPLSampleRate = [lblOPLRate.text intValue];
-    gConfig.eCDType     = (MUSICTYPE)[CDFormats indexOfObject:lblCDAudioSource.text]+MUSIC_OGG;
+    gConfig.eCDType     = (CDTYPE)[CDFormats indexOfObject:lblCDAudioSource.text];
     gConfig.iSampleRate = [lblResampleRate.text intValue];
     gConfig.wAudioBufferSize = [lblAudioBufferSize.text intValue];
     

palcfg.c
@@ -65,7 +65,7 @@ static const ConfigItem gConfigItems[PALCFG_ALL_MAX] = {
     { PALCFG_TEXTUREHEIGHT,     PALCFG_UNSIGNED, "TextureHeight",     13, MAKE_UNSIGNED(PAL_DEFAULT_TEXTURE_HEIGHT,    0,                     UINT32_MAX) },
     { PALCFG_TEXTUREWIDTH,      PALCFG_UNSIGNED, "TextureWidth",      12, MAKE_UNSIGNED(PAL_DEFAULT_TEXTURE_WIDTH,     0,                     UINT32_MAX) },
 
-	{ PALCFG_CD,                PALCFG_STRING,   "CD",                 2, MAKE_STRING("OGG") },
+	{ PALCFG_CD,                PALCFG_STRING,   "CD",                 2, MAKE_STRING("NONE") },
 	{ PALCFG_GAMEPATH,          PALCFG_STRING,   "GamePath",           8, MAKE_STRING(NULL) },
     { PALCFG_SAVEPATH,          PALCFG_STRING,   "SavePath",           8, MAKE_STRING(NULL) },
     { PALCFG_SHADERPATH,        PALCFG_STRING,   "ShaderPath",        10, MAKE_STRING(NULL) },
@@ -81,7 +81,8 @@ static const ConfigItem gConfigItems[PALCFG_ALL_MAX] = {
 	{ PALCFG_SHADER,            PALCFG_STRING,   "Shader",             6, MAKE_STRING(NULL) },
 };
 
-static const char *music_types[] = { "MIDI", "RIX", "MP3", "OGG", "RAW" };
+static const char *music_types[] = { "MIDI", "RIX", "MP3", "OGG", "OPUS", "RAW" };
+static const char *cd_types[] = { "NONE", "MP3", "OGG", "OPUS", "RAW" };
 static const char *opl_cores[] = { "MAME", "DBFLT", "DBINT", "NUKED" };
 static const char *opl_chips[] = { "OPL2", "OPL3" };
 
@@ -290,7 +291,7 @@ PAL_LoadConfig(
 	FILE     *fp;
 	ConfigValue  values[PALCFG_ALL_MAX];
 	MUSICTYPE eMusicType = MUSIC_RIX;
-	MUSICTYPE eCDType = MUSIC_OGG;
+	CDTYPE eCDType = CD_NONE;
 	OPLCORE_TYPE eOPLCore = OPLCORE_DBFLT;
 	OPLCHIP_TYPE eOPLChip = OPLCHIP_OPL2;
 	static const SCREENLAYOUT screen_layout = {
@@ -419,13 +420,13 @@ PAL_LoadConfig(
 				case PALCFG_CD:
 				{
 					if (PAL_HAS_MP3 && SDL_strncasecmp(value.sValue, "MP3", slen) == 0)
-						eCDType = MUSIC_MP3;
+						eCDType = CD_MP3;
 					else if (PAL_HAS_OGG && SDL_strncasecmp(value.sValue, "OGG", slen) == 0)
-						eCDType = MUSIC_OGG;
+						eCDType = CD_OGG;
 					else if (PAL_HAS_OPUS && SDL_strncasecmp(value.sValue, "OPUS", slen) == 0)
-						eCDType = MUSIC_OPUS;
+						eCDType = CD_OPUS;
 					else if (PAL_HAS_SDLCD && SDL_strncasecmp(value.sValue, "RAW", slen) == 0)
-						eCDType = MUSIC_SDLCD;
+						eCDType = CD_SDLCD;
 					break;
 				}
 				case PALCFG_MUSIC:
@@ -622,7 +623,7 @@ PAL_SaveConfig(
         sprintf(buf, "%s=%u\n", PAL_ConfigName(PALCFG_TEXTUREHEIGHT), gConfig.dwTextureHeight); fputs(buf, fp);
         sprintf(buf, "%s=%u\n", PAL_ConfigName(PALCFG_TEXTUREWIDTH), gConfig.dwTextureWidth); fputs(buf, fp);
 
-		sprintf(buf, "%s=%s\n", PAL_ConfigName(PALCFG_CD), music_types[gConfig.eCDType]); fputs(buf, fp);
+		sprintf(buf, "%s=%s\n", PAL_ConfigName(PALCFG_CD), cd_types[gConfig.eCDType]); fputs(buf, fp);
 		sprintf(buf, "%s=%s\n", PAL_ConfigName(PALCFG_MUSIC), music_types[gConfig.eMusicType]); fputs(buf, fp);
 		sprintf(buf, "%s=%s\n", PAL_ConfigName(PALCFG_OPL_CORE), opl_cores[gConfig.eOPLCore]); fputs(buf, fp);
 		sprintf(buf, "%s=%s\n", PAL_ConfigName(PALCFG_OPL_CHIP), opl_chips[gConfig.eOPLChip]); fputs(buf, fp);
@@ -680,7 +681,7 @@ PAL_GetConfigItem(
 		case PALCFG_WINDOWHEIGHT:      value.uValue = gConfig.dwScreenHeight; break;
 		case PALCFG_WINDOWWIDTH:       value.uValue = gConfig.dwScreenWidth; break;
 
-		case PALCFG_CD:                value.sValue = music_types[gConfig.eCDType]; break;
+		case PALCFG_CD:                value.sValue = cd_types[gConfig.eCDType]; break;
 		case PALCFG_GAMEPATH:          value.sValue = gConfig.pszGamePath; break;
 		case PALCFG_SAVEPATH:          value.sValue = gConfig.pszSavePath; break;
         case PALCFG_SHADERPATH:        value.sValue = gConfig.pszShaderPath; break;
@@ -772,11 +773,11 @@ PAL_SetConfigItem(
 		gConfig.pszShader = value.sValue && value.sValue[0] ? strdup(value.sValue) : NULL;
 		break;
 	case PALCFG_CD:
-		for (int i = 0; i < sizeof(music_types) / sizeof(music_types[0]); i++)
+		for (int i = 0; i < sizeof(cd_types) / sizeof(cd_types[0]); i++)
 		{
-			if (SDL_strcasecmp(value.sValue, music_types[i]) == 0)
+			if (SDL_strcasecmp(value.sValue, cd_types[i]) == 0)
 			{
-				gConfig.eCDType = (MUSICTYPE)i;
+				gConfig.eCDType = (CDTYPE)i;
 				return;
 			}
 		}

palcfg.h
@@ -196,7 +196,7 @@ typedef struct tagCONFIGURATION
 	INT              iSoundVolume;
 	LOGLEVEL         iLogLevel;
 	MUSICTYPE        eMusicType;
-	MUSICTYPE        eCDType;
+	CDTYPE           eCDType;
 	OPLCORE_TYPE     eOPLCore;
 	OPLCHIP_TYPE     eOPLChip;
 	WORD             wAudioBufferSize;

palcommon.h
@@ -98,11 +98,19 @@ typedef enum tagMUSICTYPE
 {
 	MUSIC_MIDI,
 	MUSIC_RIX,
-	MUSIC_MP3,
-	MUSIC_OPUS,
-	MUSIC_OGG,
-	MUSIC_SDLCD
+    MUSIC_MP3,
+    MUSIC_OGG,
+	MUSIC_OPUS
 } MUSICTYPE, *LPMUSICTYPE;
+
+typedef enum tagCDTYPE
+{
+    CD_NONE,
+    CD_MP3,
+	CD_OGG,
+    CD_OPUS,
+	CD_SDLCD
+} CDTYPE, *LPCDTYPE;
 
 typedef enum tagCODEPAGE {
 	CP_MIN = 0,

unix/unix.cpp
@@ -161,7 +161,7 @@ void InitControls()
    sprintf(buffer, "%u", gConfig.dwScreenWidth); gWidgets.windowwidth->value(buffer);
    sprintf(buffer, "%u", gConfig.dwScreenHeight); gWidgets.windowheight->value(buffer);
    gWidgets.shaderfile->value(gConfig.pszShader);
-   gWidgets.cd->value(gConfig.eCDType - MUSIC_MP3);
+   gWidgets.cd->value(gConfig.eCDType);
    gWidgets.bgm->value(gConfig.eMusicType);
    gWidgets.stereo->value(gConfig.iAudioChannels == 2 ? 1 : 0);
    sprintf(buffer, "%d", gConfig.iSampleRate); gWidgets.samplerate->value(buffer);
@@ -200,7 +200,7 @@ void SaveControls()
    gConfig.dwTextureHeight = atoi(gWidgets.textureheight->value());
    gConfig.dwScreenWidth = atoi(gWidgets.windowwidth->value());
    gConfig.dwScreenHeight = atoi(gWidgets.windowheight->value());
-   gConfig.eCDType = (MUSICTYPE)(gWidgets.cd->value() + MUSIC_MP3);
+   gConfig.eCDType = (CDTYPE)(gWidgets.cd->value());
    gConfig.eMusicType = (MUSICTYPE)(gWidgets.bgm->value());
    gConfig.iAudioChannels = gWidgets.stereo->value() ? 2 : 1;
    gConfig.iSampleRate = atoi(gWidgets.samplerate->value());
@@ -259,8 +259,8 @@ Fl_Window* InitWindow()
    gWidgets.shaderfile = new Fl_Input(100, 235, 530, 22, gLabels[lang].shaderfile);
 
    (new Fl_Box(FL_BORDER_BOX, 5, 280, 630, 130, gLabels[lang].audio))->align(FL_ALIGN_TOP);
-   (gWidgets.cd = new Fl_Choice(84, 289, 60, 22, gLabels[lang].cd))->add("MP3|OGG");
-   (gWidgets.bgm = new Fl_Choice(84, 319, 60, 22, gLabels[lang].bgm))->add("MIDI|RIX|MP3|OGG");
+   (gWidgets.cd = new Fl_Choice(84, 289, 60, 22, gLabels[lang].cd))->add("NONE|MP3|OGG|OPUS");
+   (gWidgets.bgm = new Fl_Choice(84, 319, 60, 22, gLabels[lang].bgm))->add("MIDI|RIX|MP3|OGG|OPUS");
    gWidgets.stereo = new Fl_Check_Button(lang ? 425 : 435, 320, 70, 20, gLabels[lang].stereo);
    gWidgets.samplerate = new Fl_Int_Input(570, 289, 60, 22, gLabels[lang].samplerate);
    (gWidgets.oplcore = new Fl_Choice(224, 289, 75, 22, gLabels[lang].oplcore))->add("MAME|DBFLT|DBINT|NUKED");

win32/win32.cpp
@@ -172,7 +172,7 @@ void SaveSettings(HWND hwndDlg, BOOL fWriteFile)
 	gConfig.dwTextureHeight = GetDlgItemInt(hwndDlg, IDC_TEXTUREHEIGHT, nullptr, FALSE);
 	gConfig.dwScreenWidth = GetDlgItemInt(hwndDlg, IDC_WINDOWWIDTH, nullptr, FALSE);
 	gConfig.dwScreenHeight = GetDlgItemInt(hwndDlg, IDC_WINDOWHEIGHT, nullptr, FALSE);
-	gConfig.eCDType = (MUSICTYPE)(ComboBox_GetCurSel(hwndDlg, IDC_CD) + MUSIC_MP3);
+	gConfig.eCDType = (CDTYPE)(ComboBox_GetCurSel(hwndDlg, IDC_CD));
 	gConfig.eMusicType = (MUSICTYPE)ComboBox_GetCurSel(hwndDlg, IDC_BGM);
 	gConfig.eOPLCore = (OPLCORE_TYPE)(ComboBox_GetCurSel(hwndDlg, IDC_OPL_CORE));
 	gConfig.eOPLChip = (OPLCHIP_TYPE)(gConfig.eOPLCore == OPLCORE_NUKED ? OPLCHIP_OPL3 : ComboBox_GetCurSel(hwndDlg, IDC_OPL_CHIP));
@@ -225,7 +225,7 @@ void ResetControls(HWND hwndDlg)
 	EnableDlgItem(hwndDlg, IDC_TEXTUREHEIGHT, gConfig.fEnableGLSL ? TRUE : FALSE);
 	EnableDlgItem(hwndDlg, IDC_BRSHADER, gConfig.fEnableGLSL ? TRUE : FALSE);
 
-	ComboBox_SetCurSel(hwndDlg, IDC_CD, gConfig.eCDType - MUSIC_MP3);
+	ComboBox_SetCurSel(hwndDlg, IDC_CD, gConfig.eCDType);
 	ComboBox_SetCurSel(hwndDlg, IDC_BGM, gConfig.eMusicType);
 	ComboBox_SetCurSel(hwndDlg, IDC_OPL_CORE, gConfig.eOPLCore);
 	ComboBox_SetCurSel(hwndDlg, IDC_OPL_CHIP, gConfig.eOPLChip);
@@ -276,13 +276,16 @@ INT_PTR InitProc(HWND hwndDlg, HWND hwndCtrl, LPARAM lParam)
 		}
 	}
 
+	ComboBox_AddString(hwndDlg, IDC_CD, TEXT("None"));
 	ComboBox_AddString(hwndDlg, IDC_CD, TEXT("MP3"));
 	ComboBox_AddString(hwndDlg, IDC_CD, TEXT("OGG"));
+	ComboBox_AddString(hwndDlg, IDC_CD, TEXT("OPUS"));
 
 	ComboBox_AddString(hwndDlg, IDC_BGM, TEXT("MIDI"));
 	ComboBox_AddString(hwndDlg, IDC_BGM, TEXT("RIX"));
 	ComboBox_AddString(hwndDlg, IDC_BGM, TEXT("MP3"));
 	ComboBox_AddString(hwndDlg, IDC_BGM, TEXT("OGG"));
+	ComboBox_AddString(hwndDlg, IDC_BGM, TEXT("OPUS"));
 
 	ComboBox_AddString(hwndDlg, IDC_OPL_CORE, TEXT("MAME"));
 	ComboBox_AddString(hwndDlg, IDC_OPL_CORE, TEXT("DBFLT"));

commit e20a20ab660a99db8bf3c95e56ff138c0091f333

bugfix: android settings GUI crashing introduced in fac3226, fix #175

android/app/src/main/java/com/sdlpal/sdlpal/SettingsActivity.java
@@ -316,7 +316,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     protected int findMatchedStringIndex(String value, String[] values, int defaultIndex) {
         for(int i = 0; i < values.length; i++) {
-            if (values[i].equals(value))
+            if (values[i].equalsIgnoreCase(value))
                 return i;
         }
         return defaultIndex;

android/app/src/main/res/values/arrays.xml
@@ -12,14 +12,17 @@
         <item>8192</item>
     </string-array>
     <string-array name="cd_format" translatable="false">
+        <item>None</item>
         <item>MP3</item>
         <item>OGG</item>
+        <item>OPUS</item>
     </string-array>
     <string-array name="music_format" translatable="false">
         <item>MIDI</item>
         <item>RIX</item>
         <item>MP3</item>
         <item>OGG</item>
+        <item>OPUS</item>
     </string-array>
     <string-array name="opl_core" translatable="false">
         <item>MAME</item>