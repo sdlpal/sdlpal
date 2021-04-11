b0298c7011ec0f325dbc78ef9ad41220c666685c
palxex <ex+home@palx.org>
2021-04-11 12:57:53 +0800

    seperate scripts as submodule

--- scripts/bumpversion.sh
+++
@@ -1,2 +0,0 @@
-NEWY=$(date "+%Y")
-! [ -z $NEWY ] && find . \( -iname '*.h' -or -iname '*.c' -or -iname '*.cpp' -or -iname '*.hpp' -or -iname '*.m' -or -iname '*.mm' -or -iname '*.java' \) -exec gsed -i "s/(c) 2011-[0-9]*, SDLPAL/(c) 2011-$NEWY, SDLPAL/g" {} \;
