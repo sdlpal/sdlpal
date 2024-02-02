7b852350796605c01bfffd7b77550d16bd899f3e
liuzhier <2298813582@qq.com>
2024-02-02 23:12:01 +0800

    fixbug: in the PlayerInfoBox menu cursor that is different from the original PAL when using single player healing spells.

--- ui.h
+++ ui.h
@@ -27,9 +27,9 @@
 #define CHUNKNUM_SPRITEUI                  9
 
 #define MENUITEM_COLOR                     0x4F
-#define MENUITEM_COLOR_INACTIVE            0x1C
+#define MENUITEM_COLOR_INACTIVE            0x18
 #define MENUITEM_COLOR_CONFIRMED           0x2C
-#define MENUITEM_COLOR_SELECTED_INACTIVE   0x1F
+#define MENUITEM_COLOR_SELECTED_INACTIVE   0x1C
 #define MENUITEM_COLOR_SELECTED_FIRST      0xF9
 #define MENUITEM_COLOR_SELECTED_TOTALNUM   6
 
@@ -108,6 +108,8 @@
 
 #define SPRITENUM_SLASH                    39
 #define SPRITENUM_ITEMBOX                  70
+#define SPRITENUM_CURSOR_YELLOW_UP         66
+#define SPRITENUM_CURSOR_UP                67
 #define SPRITENUM_CURSOR_YELLOW            68
 #define SPRITENUM_CURSOR                   69
 #define SPRITENUM_PLAYERINFOBOX            18
