27166205158587fae1ea27549cadad231fd06d12
PalAlexx <47782892+PalAlexx@users.noreply.github.com>
2025-10-07 15:30:05 +0800

    fix-script -0x0086
    you prevent the case where the required count is 0. Previously, the condition if (y < 0) could never be true, so the jump never executed.
     Now, if the script doesn’t specify a required count, it defaults to 1. That means “jump if nobody has this item equipped” works as intended.

--- script.c
+++ script.c
@@ -1,4 +1,4 @@
-/* -*- mode: c; tab-width: 4; c-basic-offset: 4; c-file-style: "linux" -*- */
+﻿/* -*- mode: c; tab-width: 4; c-basic-offset: 4; c-file-style: "linux" -*- */
 //
 // Copyright (c) 2009-2011, Wei Mingzhi <whistler_wmz@users.sf.net>.
 // Copyright (c) 2011-2024, SDLPAL development team.
@@ -2519,6 +2519,8 @@ PAL_InterpretInstruction(
       //
       // Jump if the specified item is not equipped
       //
+      if (pScript->rgwOperand[1] == 0) 
+           pScript->rgwOperand[1] = 1;
       y = 0;
       for (i = 0; i <= gpGlobals->wMaxPartyMemberIndex; i++)
       {
